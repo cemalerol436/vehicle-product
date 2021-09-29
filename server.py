7694e7f3d6d7eac5450a59ce4f3b9b7e904ff394
Your Name <you@example.com>
2021-09-29 23:25:55 +0300

    son hali

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 import pymysql.cursors
 
-db = pymysql.connect(host='sql5.freemysqlhosting.net',
-                             user='sql5436558',
-                             password='4wlHZNFSWd',
-                             db='sql5436558',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-baglanti = db.cursor()
 
+db = pymysql.connect(host='sql5.freemysqlhosting.net',
+                     user='sql5436558',
+                     password='4wlHZNFSWd',
+                     db='sql5436558',
+                     charset='utf8mb4',
+                     cursorclass=pymysql.cursors.DictCursor)
+baglanti = db.cursor()
 
 app = Flask(__name__)
 
@@ -28,4 +28,33 @@ def brands():
 
 @app.route('/get-models')
 def models():
-    return 'Hello from Server'
+    brand = request.args.get("brand")
+    baglanti.execute('SELECT id, name, start_year, end_year FROM vehicles WHERE brand=%s', [brand])
+    modeller = baglanti.fetchall()
+    list = []
+    for k in modeller:
+        list.append({"id": k.get("id"),
+                     "model":k.get("name"),
+                     "modelstart":k.get("start_year"),
+                     "modelend":k.get("end_year")
+                     })
+
+    return {
+        "data": list
+    }
+@app.route('/get-products')
+def products():
+    vehicle = request.args.get("vehicle")
+    baglanti.execute('SELECT id, name, start_year, end_year FROM vehicles WHERE brand=%s', [vehicle])
+    products = baglanti.fetchall()
+    list = []
+    for k in products:
+        list.append({"id": k.get("id"),
+                     "bracket_group":k.get("bracket_group"),
+                     "product_name":k.get("product_name"),
+                     "image":k.get("image")
+                     })
+
+    return {
+        "data": list
+    }
